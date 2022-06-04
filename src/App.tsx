019ed9e4ca3e752c8ef2a98bcc3e89b8361c2ab6
fhariza29 <fharizaalghifera18634@gmail.com>
2022-06-04 21:57:31 +0700

    download tailwind

---
+++ src/App.tsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import './index.css';
+
+function App() {
+  return (
+    <>
+    <div className='bg-pink-600  text-center mb-2'>Halo Dunia </div>
+    <ul>
+      <button >
+      <li className='rounded bg-slate-400 mb-2 hover:bg-pink-500'>Daftar Jurusan</li>
+      <li className='rounded bg-slate-400 mb-2'>Daftar Kelas</li>
+      <li className='rounded bg-slate-400 '>Daftar Nama Siswa</li>
+      </button>
+      
+    </ul>
+    </>
+    
+  );
+}
+
+export default App;
