0dd8bcbfe8197605b1951d5ef35f97e58a4ca386
fhariza29 <fharizaalghifera18634@gmail.com>
2022-06-08 20:42:19 +0700

    change bg

--- src/App.tsx
+++ src/App.tsx
@@ -7,13 +7,10 @@ function App() {
     <div className='bg-blue-600  text-center mb-2'>Halo Dunia </div>
     <ul>
       <button >
-      <li className='rounded bg-slate-400 mb-2 hover:bg-blue-700'>Daftar Jurusan</li>
+      <li className='rounded bg-slate-400 mb-2 hover:bg-gray-700'>Daftar Jurusan</li>
       <li className='rounded bg-slate-400 mb-2'>Daftar Kelas</li>
       <li className='rounded bg-slate-400 mb-2'>Daftar Nama Siswa</li>
-      <li className='rounded bg-slate-400 mb-2 '>Daftar Guru</li>
-      <li className='rounded bg-slate-400 mb-2'>Daftar Kelas</li>
-      <li className='rounded bg-slate-400 mb-2'>Daftar Nama Siswa</li>
-      <li className='rounded bg-slate-400 mb-2 '>Daftar Guru</li>
+      
  
       </button>
       
