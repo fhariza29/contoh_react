417e12b652a742aa148ee77485e0a358e8f1f3a8
fhariza29 <fharizaalghifera18634@gmail.com>
2022-06-08 20:36:03 +0700

    menambahkan li

--- src/App.tsx
+++ src/App.tsx
@@ -7,10 +7,11 @@ function App() {
     <div className='bg-blue-600  text-center mb-2'>Halo Dunia </div>
     <ul>
       <button >
-      <li className='rounded bg-slate-400 mb-2 hover:bg-pink-500'>Daftar Jurusan</li>
+      <li className='rounded bg-slate-400 mb-2 hover:bg-blue-700'>Daftar Jurusan</li>
       <li className='rounded bg-slate-400 mb-2'>Daftar Kelas</li>
       <li className='rounded bg-slate-400 mb-2'>Daftar Nama Siswa</li>
-      <li className='rounded bg-slate-400 '>Daftar Guru</li>
+      <li className='rounded bg-slate-400 mb-2 '>Daftar Guru</li>
+      <li className='rounded bg-slate-400 '>Daftar Guru BK</li>
       </button>
       
     </ul>
